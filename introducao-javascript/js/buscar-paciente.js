25da4853d9ac27455a9c6829112db3ec7cd17aec
Rodrigo Cardoso de Oliveira <rodrigo.cardoso.oliveira@gmail.com>
2019-02-15 13:51:03 -0200

    capitulo9

--- introducao-javascript/js/buscar-paciente.js
+++ introducao-javascript/js/buscar-paciente.js
@@ -3,20 +3,28 @@ var botaoBuscar = document.querySelector("#buscar-paciente");
 botaoBuscar.addEventListener("click", function(){
     console.log("buscando pacientes");
 
-    // http://api-pacientes.herokuapp.com/pacientes
-
     var xhr = new XMLHttpRequest();
 
     xhr.open("GET", "http://api-pacientes.herokuapp.com/pacientes");
     
     xhr.addEventListener("load", function(){
-        var resposta = xhr.responseText;
 
-        var pacientes = JSON.parse(resposta);
+        if (xhr.status == 200) {
+            var erroAjax = document.querySelector("#erro-ajax");
+            erroAjax.classList.add("invisivel");
+            var resposta = xhr.responseText;
 
-        pacientes.forEach(function(paciente){
-            adicionaPacienteNaTabela(paciente);
-        });
+            var pacientes = JSON.parse(resposta);
+
+            pacientes.forEach(function(paciente){
+                adicionaPacienteNaTabela(paciente);
+            });
+        } else {
+            console.log(xhr.status);
+            console.log(xhr.responseText);
+            var erroAjax = document.querySelector("#erro-ajax");
+            erroAjax.classList.remove("invisivel");
+        }
 
     });
     
